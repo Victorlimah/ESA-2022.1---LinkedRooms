fd9b1a6cd7ee6830ae5f2175c91cce4616e4825b
carlos <caarlos.albuquerq@gmail.com>
2022-12-04 18:55:25 -0300

    :chore: add nodemailer connection

--- linkedrooms-back/src/controllers/authController.ts
+++ linkedrooms-back/src/controllers/authController.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
+import sendEmail from "../services/emailService";
 
 export async function signin(req: Request, res: Response) {
+  const {email} = req.body;
 
+  sendEmail(email)
 }
--- linkedrooms-back/src/services/emailService.ts
+++ linkedrooms-back/src/services/emailService.ts
@@ -1,4 +1,34 @@
+import dotenv from "dotenv";
+import { MailService } from "@sendgrid/mail";
+dotenv.config();
 
-export default function verifyEmail() {
+export default function sendEmail(email: String) {
+  const sgMail = new MailService();
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
+  const msg = {
+    to: email, // Change to your recipient
+    from: 'linkedrooms0@gmail.com', // Change to your verified sender
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<button>and easy to do anywhere, even with Node.js />',
+  }
+
+  send(msg);  
 }
+
+function send(msg) {
+  if (msg.to.includes('dcx.ufpb.br')) {
+    sgMail
+      .send(msg)
+      .then(() => {
+      console.log('Email sent')
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  } else {
+    console.log("Email inválido! Tente usar um email dcx");
+  }
+}
+
