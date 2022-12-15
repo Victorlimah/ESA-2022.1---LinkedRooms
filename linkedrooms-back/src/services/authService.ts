1ac313e47fe19faf909cf03c2a7d02770e72c845
carlos <caarlos.albuquerq@gmail.com>
2022-12-14 22:26:48 -0300

    chore: change the lib used in the otp generation

--- linkedrooms-back/src/services/authService.ts
+++ linkedrooms-back/src/services/authService.ts
@@ -1,10 +1,9 @@
 import * as repository from "../repositories/authRepository.js";
 import { processUserName, sendEmail } from "../utils/emailUtils.js";
-import { v4 as uuid} from "uuid";
 import { User } from "../models/dataDto.js";
 import dotenv from "dotenv";
 import { conflictError, notFoundError, unauthorizedError } from "../utils/errorUtils.js";
-import { encrypt, generateToken, decrypt } from "../utils/passwordUtils.js";
+import { encrypt, generateToken, decrypt, generateOTP } from "../utils/passwordUtils.js";
 
 dotenv.config();
 
@@ -12,7 +11,6 @@ export async function createUser({ email, password }) {
   const userExists = await repository.search("email", email);
   if (userExists) throw conflictError("User already exists");
 
-  const authorizationCode = uuid();
   const name = processUserName(email);
   const encryptedPassword = encrypt(password);
 
@@ -21,7 +19,7 @@ export async function createUser({ email, password }) {
     name,
     password: encryptedPassword,
     role: process.env.ROLE,
-    code: authorizationCode
+    code: generateOTP()
   };
 
   repository.create(newUser);
--- linkedrooms-back/src/utils/passwordUtils.ts
+++ linkedrooms-back/src/utils/passwordUtils.ts
@@ -1,6 +1,8 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import otpGenerator from "otp-generator";
 import dotenv from "dotenv";
+dotenv.config();
 
 export function encrypt(password: string) {
   return bcrypt.hashSync(password, 10);
@@ -14,4 +16,8 @@ export function generateToken(dataToEncrypt: object) {
   const keyJWT = process.env.JWT_SECRET;
   const token = jwt.sign(dataToEncrypt, keyJWT);
   return token;
+}
+
+export function generateOTP() {
+  return otpGenerator.generate(6, {lowerCaseAlphabets: false, specialChars: false});
 }
